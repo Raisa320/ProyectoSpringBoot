c179a8974c6d4501b68a8dbec97ba4525eec0b4b
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-03-22 03:21:30 -0500

    Creacion de perfil de usuario, modificacion de sus datos

--- src/main/java/pds/wabbit/Repositorios/IUsuarioRepositorio.java
+++ src/main/java/pds/wabbit/Repositorios/IUsuarioRepositorio.java
@@ -1,5 +1,7 @@
 package pds.wabbit.Repositorios;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +11,8 @@ public interface IUsuarioRepositorio extends JpaRepository<Usuario, String> {
 
     @Query("SELECT d from Usuario d WHERE d.email=:email AND d.eliminado=false")
     public Usuario buscarUsuarioPorEmail(@Param("email") String email);
+    
+    @Query("SELECT d from Usuario d WHERE d.eliminado=false")
+    public Page<Usuario> usuariosActivos(Pageable pageable);
 
 }
